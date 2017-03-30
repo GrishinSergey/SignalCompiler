8d4a9a3e6b41e7d777e943374821af68160fd32d
SergeyGrishin <aservasya@yandex.ua>
2017-03-30 22:44:43 +0300

    fill test-file and TestRunnerClass

--- src/scanner/Scanner.java
+++ src/scanner/Scanner.java
@@ -15,27 +15,23 @@ public class Scanner {
 
     private BufferedReader reader;
     private ArrayList<Token> tokens;
+    private int chr;
+    private int lineNumber;
 
     public Scanner(String pathToFile) throws IOException {
         tokens = new ArrayList<>();
         reader = new BufferedReader(new FileReader(pathToFile));
+        chr = reader.read();
+        lineNumber = 1;
     }
 
     public Scanner generateTokenList() throws IOException {
-        int chr = reader.read(), lineNumber = 1, tokenCode;
-        String token = "";
+        int tokenCode;
+        String token;
         do {
             try {
-                if (isLetter(chr)) {
-                    while (!isDelimiter(chr) && !isWhitespace(chr) && chr != -1) {
-                        if (isLetter(chr) || Character.isDigit(chr)) {
-                            token += (char) chr;
-                            chr = reader.read();
-                        }
-                        else {
-                            throw new ScannerException();
-                        }
-                    }
+                if (isLetter()) {
+                    token = getIdentifier();
                     if ((tokenCode = KeyWordsTable.getInstance().getTokenCode(token)) != -1) {
                         addToken(tokenCode, lineNumber);
                     } else if ((tokenCode = IdentifiersTable.getInstance().getTokenCode(token)) != -1) {
@@ -43,7 +39,7 @@ public class Scanner {
                     } else {
                         addToken(IdentifiersTable.getInstance().add(token), lineNumber);
                     }
-                } else if (isDelimiter(chr)) {
+                } else if (isDelimiter()) {
                     if (chr != 40) {
                         addToken(DelimitersTable.getInstance().getTokenCode(Character.toString((char) chr)), lineNumber);
                         chr = reader.read();
@@ -52,31 +48,15 @@ public class Scanner {
                         token = Character.toString((char) chr);
                         chr = reader.read();
                         if (chr == 42) {
-                            addToken(DelimitersTable.getInstance().getTokenCode("(*"), lineNumber);
-                            while (chr != -1) {
-                                if (chr == 42) {
-                                    chr = reader.read();
-                                    if (chr == 41) {
-                                        addToken(DelimitersTable.getInstance().getTokenCode("*)"), lineNumber);
-                                        break;
-                                    }
-                                }
-                                else {
-                                    if (isNewLine(chr)) {
-                                        lineNumber++;
-                                    }
-                                    chr = reader.read();
-                                }
-                            }
-                            chr = reader.read();
+                            getComment();
                         }
                         else {
                             addToken(DelimitersTable.getInstance().getTokenCode(token), lineNumber);
                         }
                     }
-                } else if (isWhitespace(chr)) {
-                    while (isWhitespace(chr)) {
-                        if (isNewLine(chr)) {
+                } else if (isWhitespace()) {
+                    while (isWhitespace()) {
+                        if (isNewLine()) {
                             lineNumber++;
                         }
                         chr = reader.read();
@@ -85,34 +65,68 @@ public class Scanner {
                     throw new ScannerException();
                 }
             } catch (ScannerException se) {
-                chr = readWhile(chr);
+                chr = readWhile();
             }
-            token = "";
         } while (chr != -1);
         reader.close();
         return this;
     }
 
-    private int readWhile(int chr) throws IOException {
-        while (!isDelimiter(chr) && !isWhitespace(chr) && chr != -1) {
+    private String getIdentifier() throws ScannerException, IOException {
+        String token = "";
+        while (!isDelimiter() && !isWhitespace() && chr != -1) {
+            if (isLetter() || Character.isDigit(chr)) {
+                token += (char) chr;
+                chr = reader.read();
+            }
+            else {
+                throw new ScannerException();
+            }
+        }
+        return token;
+    }
+
+    private void getComment() throws IOException {
+        addToken(DelimitersTable.getInstance().getTokenCode("(*"), lineNumber);
+        while (chr != -1) {
+            if (chr == 42) {
+                chr = reader.read();
+                if (chr == 41) {
+                    addToken(DelimitersTable.getInstance().getTokenCode("*)"), lineNumber);
+                    break;
+                }
+            }
+            else {
+                if (isNewLine()) {
+                    lineNumber++;
+                }
+                chr = reader.read();
+            }
+        }
+        chr = reader.read();
+
+    }
+
+    private int readWhile() throws IOException {
+        while (!isDelimiter() && !isWhitespace() && chr != -1) {
             chr = reader.read();
         }
         return chr;
     }
 
-    private boolean isNewLine(int chr) throws IOException {
+    private boolean isNewLine() throws IOException {
         return chr == 10 || chr == 13 && reader.read() == 10 || chr == 13;
     }
 
-    private boolean isLetter(int chr) {
+    private boolean isLetter() {
         return chr >= 65 && chr <= 90;
     }
 
-    private boolean isDelimiter(int chr) {
+    private boolean isDelimiter() {
         return chr == 44 || chr == 58 || chr == 59 || chr == 40 || chr == 41;
     }
 
-    private boolean isWhitespace(int chr) {
+    private boolean isWhitespace() {
         return chr == 9 || chr == 32 || chr == 10 || chr == 13 || chr == 11;
     }
 

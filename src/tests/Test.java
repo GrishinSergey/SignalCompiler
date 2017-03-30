1d8008f7fe1c37f8fd7f568b2cb4cdebffc9bc31
SergeyGrishin <aservasya@yandex.ua>
2017-03-30 22:22:13 +0300

    fill test-file and TestRunnerClass

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -11,13 +11,14 @@ public class Test {
     private static Printer printer;
 
     public static void main(String[] args) {
-        runTest("Cyrillic test", "/home/sergey/Java/projects/SignalCompiler/src/tests/code/cyrillic.sig");
-        runTest("Whitespace test", "/home/sergey/Java/projects/SignalCompiler/src/tests/code/whitespaces.sig");
+        runTest("Cyrillic test", "/home/sergey/Java/projects/SignalCompiler/src/tests/code/test.sig");
+//        runTest("Cyrillic test", "/home/sergey/Java/projects/SignalCompiler/src/tests/code/cyrillic.sig");
+//        runTest("Whitespace test", "/home/sergey/Java/projects/SignalCompiler/src/tests/code/whitespaces.sig");
         runTest("Usual comment test", "/home/sergey/Java/projects/SignalCompiler/src/tests/code/comments-1.sig");
         runTest("Non-closed comments test", "/home/sergey/Java/projects/SignalCompiler/src/tests/code/comments-2.sig");
         runTest("Without spaces comment test", "/home/sergey/Java/projects/SignalCompiler/src/tests/code/comments-3.sig");
-        runTest("Correct test", "/home/sergey/Java/projects/SignalCompiler/src/tests/code/correct-test.sig");
-        runTest("Errors contains test", "/home/sergey/Java/projects/SignalCompiler/src/tests/code/errors-contains-test.sig");
+//        runTest("Correct test", "/home/sergey/Java/projects/SignalCompiler/src/tests/code/correct-test.sig");
+//        runTest("Errors contains test", "/home/sergey/Java/projects/SignalCompiler/src/tests/code/errors-contains-test.sig");
     }
 
     private static void runTest(String testName, String filePath) {
